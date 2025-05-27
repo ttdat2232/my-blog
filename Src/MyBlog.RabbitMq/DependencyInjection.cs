c42a0a456c34a13677cbe984f3b711fed2f3580a
Truong <dd220302@gmail.com>
2025-05-27 23:32:20 +0700

    feat(format): format with csharpier

--- Src/MyBlog.RabbitMq/DependencyInjection.cs
+++ Src/MyBlog.RabbitMq/DependencyInjection.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-
 using MyBlog.Core.Services.Cache;
 using MyBlog.Core.Services.Messages;
 using MyBlog.RabbitMq.Configurations;
